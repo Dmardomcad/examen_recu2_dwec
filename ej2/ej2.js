43a3f60bcb42d43d64fd93f5db4aa1b97bf2508d
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-03-06 11:38:00 +0100

    .

--- ej2/ej2.js
+++ ej2/ej2.js
@@ -1,16 +1,19 @@
 function isLeapYear(num1, num2){
-
+    let leapYears = 0
     if (isNaN(num1 || num2)|| num1 < 2001 || num2 > 2500){
         console.log('Error, número no válido!')
         return
     }
     //Se redondean los números
     Math.round(num1) && Math.round(num2)
-    /*     console.log(Math.round(num1))
-    console.log(Math.round(num2)) */
-    
-
-
+    while(num1 < num2){    
+        if(num1 % 4 === 0 || num2 % 4 === 0){
+            leapYears++
+            num1++
+        }
+        num1++
+    }
+    console.log(`Hay ${leapYears} años bisiestos entre los años introducidos`)
 }
 // Comprobamos que no se acepta un número que no esté entre los rangos que se especifican
 isLeapYear(1999,2300)
